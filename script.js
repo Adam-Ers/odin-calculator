643ff3d178416d4e6be167af584744591a3c7259
Adam Erstad <adamerstad@gmail.com>
2024-02-26 21:30:28 -0800

    Changes:
    * Buttons change color while clicked.
    * Clear button functionality added (click to delete last number, hold click to clear completely)
    * +/- button functionality added, allowing toggling if the number is negative.

--- script.js
+++ script.js
@@ -1,26 +1,55 @@
 const screen = document.querySelector('#screen');
 
-const max_characters = 9;
+const maxCharacters = 9;
+const clearLongPressDelay = 1000;
+let clearClicked = 0;
 
-function add_number(num) {
+function addNumber(num) {
     if (screen.textContent == '0')
     {
         screen.textContent = num;
     }
     else
     {
-        if (screen.textContent.length >= max_characters)
+        if (screen.textContent.length >= maxCharacters)
         {
-            screen.textContent = screen.textContent.slice(1, max_characters);
+            screen.textContent = screen.textContent.slice(1, maxCharacters);
         }
         screen.textContent += num;
     }
 }
 
+function clearMouseDown() {
+    clearClicked = Date.now();
+}
+
+function clearNumber() {
+    if (screen.textContent === '0') { return; }
+    if (Date.now() - clearClicked >= clearLongPressDelay) { screen.textContent = '0'; }
+    else {
+        screen.textContent = screen.textContent.slice(0, screen.textContent.length - 1);
+    }
+}
+
+function toggleNegative()
+{
+    let isNegative = screen.textContent.charAt(0) === '-'
+    if (isNegative) {
+        screen.textContent = screen.textContent.slice(1, screen.textContent.length);
+    }
+    else {
+        if (screen.textContent.length == maxCharacters) { screen.textContent = screen.textContent.slice(1, screen.textContent.length); }
+        screen.textContent = '-' + screen.textContent;
+    }
+}
+
 function start() {
     document.querySelectorAll('.number').forEach( element => {
-        element.addEventListener('click', e => { add_number(e.target.id); });
-    })
+        element.addEventListener('click', e => { addNumber(e.target.id); });
+    });
+    document.querySelector('#clear').addEventListener('mousedown', clearMouseDown);
+    document.querySelector('#clear').addEventListener('mouseup', clearNumber);
+    document.querySelector('#plusMinus').addEventListener('click', toggleNegative);
 }
 
 window.onload = start
