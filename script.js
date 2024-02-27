d761b023fe1bcc1d388651e3da7957ca90e4929d
Adam Erstad <adamerstad@gmail.com>
2024-02-26 21:51:29 -0800

    Changes:
    * Clear long press delay changed to 500ms from 1000ms.
    * Clear button now more clearly (heh) shows that it is an all clear if you hold it.
    * Decimal functionality added.
    * Percent button functionality added.
    * Adding numbers after hitting the character limit while the number is negative now works properly.

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 const screen = document.querySelector('#screen');
 
 const maxCharacters = 9;
-const clearLongPressDelay = 1000;
+const clearLongPressDelay = 500;
 let clearClicked = 0;
 
 function addNumber(num) {
     if (screen.textContent == '0')
     {
         screen.textContent = num;
+        if (num === '.') { screen.textContent = "0."; }
     }
     else
     {
         if (screen.textContent.length >= maxCharacters)
         {
-            screen.textContent = screen.textContent.slice(1, maxCharacters);
+            if (screen.textContent.charAt(2) == '.') { return; }
+            else if (screen.textContent.charAt(0) == '-') { screen.textContent = '-' + screen.textContent.slice(2, maxCharacters); }
+            else { screen.textContent = screen.textContent.slice(1, maxCharacters); }
         }
         screen.textContent += num;
     }
+}
+
+function addDecimal() {
+    if (screen.textContent.includes('.')) { return; }
+    else { addNumber('.'); }
 }
 
 function clearMouseDown() {
@@ -43,13 +51,23 @@ function toggleNegative()
     }
 }
 
+function toPercent() {
+    let num = parseFloat(screen.textContent);
+    num *= 0.01
+    let numString = num.toString();
+    if (numString.length > maxCharacters) { numString = numString.slice(0, maxCharacters); }
+    screen.textContent = numString;
+}
+
 function start() {
     document.querySelectorAll('.number').forEach( element => {
         element.addEventListener('click', e => { addNumber(e.target.id); });
     });
+    document.querySelector('#decimal').addEventListener('click', addDecimal);
     document.querySelector('#clear').addEventListener('mousedown', clearMouseDown);
     document.querySelector('#clear').addEventListener('mouseup', clearNumber);
     document.querySelector('#plusMinus').addEventListener('click', toggleNegative);
+    document.querySelector('#percent').addEventListener('click', toPercent);
 }
 
 window.onload = start
